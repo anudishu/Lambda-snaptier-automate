3bf5811b562857258243cc438d9afeca4cd20923
Your Name <you@example.com>
2024-07-01 16:57:37 +0530

    lambda

---
+++ create-snap.py
@@ -0,0 +1,138 @@
+import boto3
+import os
+from datetime import datetime, date, timedelta
+
+Begindatestring = date.today()
+DeleteOn = Begindatestring + timedelta(days=97)
+ArchiveOn = Begindatestring + timedelta(days=14)
+
+
+
+# Replace with your SNS topic ARN
+sns_topic_arn = "<replace with your SNS Topic ARN"
+
+# Replace with your sender email address
+#sender_email = "lyfedge799@gmail.com"
+
+# Create SNS client
+client = boto3.client('ec2')
+sns_client = boto3.client('sns')
+
+
+def lambda_handler(event, context):
+    # Get the paginator for describing volumes
+    paginator = client.get_paginator('describe_volumes')
+
+    # Create a response iterator to iterate through the volumes
+    response_iterator = paginator.paginate(
+        Filters=[
+            {
+                'Name': 'tag:EBS_lambda',
+                'Values': [
+                    'backup',
+                ]
+            },
+        ]
+    )
+
+    # Initialize empty lists for ebsnm and ebslist
+    ebsnm = []
+    ebslist = []
+
+    # Initialize empty list to store snapshot details
+    snapshot_details = []
+
+    # Iterate through the response iterator
+    for obj in response_iterator:
+        try:
+            for vol in obj['Volumes']:  
+                # Add volume ID to ebslist
+                ebslist.append(vol['VolumeId'])
+                print("List of volume id {}".format(vol['VolumeId'])) 
+                
+                
+
+                # Get device name from attachments
+                devicenm = None
+                for devnm in vol['Attachments']:
+                    devicenm = devnm['Device']
+
+                # Get snapshot name from tags
+                ebsnm = None
+                for tag in vol['Tags']:
+                    if tag['Key'] == 'Name' or tag['Key'] == 'name':
+                        ebsnm = tag['Value']
+
+                # Get current time
+                create_time = datetime.now()
+
+                # Create a snapshot with appropriate tags
+                sanp = client.create_snapshot(
+                    Description="EBSName-" + ebsnm + "-VolumeId-" + vol['VolumeId'],
+                    VolumeId=vol['VolumeId'],
+                    TagSpecifications=[
+                        {
+                            'ResourceType': 'snapshot',
+                            'Tags': [
+                                {
+                                    'Key': 'DeleteOn',
+                                    'Value': str(DeleteOn),
+                                },
+                                {
+                                    'Key': 'DeviceName',
+                                    'Value': devicenm,
+                                },
+                                {
+                                    'Key': 'Time_Created',
+                                    'Value': str(create_time),
+                                },
+                                {
+                                    'Key': 'EBS_lambda',
+                                    'Value': "backup",
+                                },
+                                {
+                                    'Key': 'ArchiveOn',
+                                    'Value': str(ArchiveOn),
+                                },
+                                {
+                                    'Key': 'Name',
+                                    'Value': ebsnm,
+                                },
+                            ]
+                        }
+                    ]
+                )
+
+                # Append snapshot details to the list
+                snapshot_details.append({
+                    'SnapshotId': sanp['SnapshotId'],
+                    'SnapshotName': sanp['Description'],
+                })
+
+        except Exception as e:
+            print("Error occurred:", e)   #If any errors occur during processing, it catches the error and prints an error message
+
+    # Send SNS notification with snapshot details
+    if len(snapshot_details) > 0:
+        email_subject = "Snapshot Created on {} - Count: {}".format(datetime.now().strftime("%Y-%m-%d"), len(snapshot_details))
+        email_body = """
+        The following snapshots have been created:
+
+        """.format(snapshot_details)
+
+        for i, snapshot in enumerate(snapshot_details):
+            email_body += "{}. Snapshot ID: {}\n    Snapshot Name: {}\n\n".format(i + 1, snapshot['SnapshotId'], snapshot['SnapshotName'])
+
+        email_body += """
+
+        Please review and retain these snapshots for backup purposes.
+
+        """
+
+        sns_client.publish(
+            TopicArn=sns_topic_arn,
+            Message=email_body,
+            Subject=email_subject,
+        )
+
+    print("Found %d Volumes that need backing up" % len(ebslist))
---
+++ test-change-tier.py
@@ -0,0 +1,82 @@
+# This script is also good. it send email in nice format. tested.
+import boto3
+import datetime
+from botocore.exceptions import ClientError
+
+# Create an SNS client
+sns_client = boto3.client('sns')
+ec2 = boto3.client('ec2')
+
+# Set the SNS topic ARN
+sns_topic_arn = "arn:aws:sns:us-east-1:810941123870:test_isos_CloudWatch_Alarms_Topic"
+
+# Get the current date in YYYY-MM-DD format
+currentDate = datetime.datetime.now().strftime("%Y-%m-%d")
+
+# Initialize an empty list to store snapshot details
+snaplist = []
+
+def lambda_handler(event, context):
+    # Create a filter to find snapshots with tag key ArchiveOn and value current date
+    filters = [
+        {
+            'Name': 'tag:ArchiveOn',
+            'Values': [
+                currentDate,
+            ],
+        },
+    ]
+
+    # Get the list of snapshots matching the filter
+    paginator = ec2.get_paginator('describe_snapshots')
+    response_iterator = paginator.paginate(
+        Filters=filters,
+    )
+
+    # Iterate over the snapshots and process them
+    for obj in response_iterator:
+        for snap in obj['Snapshots']:
+            snapshot_id = snap['SnapshotId']
+            snapshot_name = snap.get('Description', 'N/A')
+
+            snaplist.append({
+                'SnapshotId': snapshot_id,
+                'SnapshotName': snapshot_name,
+            })
+
+            # Archive the snapshot
+            try:
+                archivesnap = ec2.modify_snapshot_tier(
+                    SnapshotId=snapshot_id,
+                    StorageTier='archive',
+                    # DryRun=True # Uncomment this line to test without archiving
+                )
+                print("Archived snapshot id {}".format(snapshot_id))
+            except ClientError as e:
+                print(e.response['Error']['Message'])
+
+    # Prepare the email message
+    subject = "List of Snapshots to Archive on {}".format(currentDate)
+    body = "The following snapshots are scheduled for archiving:\n\n"
+
+    # Create a formatted table in email body
+    body += "\t{:<50} {:<100}\n".format('Snapshot ID', 'Snapshot Name')
+    for snapshot_info in snaplist:
+        body += "\t{:<50} {:<100}\n".format(
+            snapshot_info['SnapshotId'],
+            snapshot_info['SnapshotName'],
+        )
+
+    # Send the email notification via SNS
+    try:
+        sns_client.publish(
+            TopicArn=sns_topic_arn,
+            Subject=subject,
+            Message=body,
+        )
+        print("Sent email notification with list of snapshots to archive")
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+print("List of Snapshots to Archive:")
+print(snaplist)
